c5c80ecab5fc6f0a7c48d0551d7e7dde5bc27498
Bryan Hann <nobody@nowhere>
2020-08-09 17:16:07 +0800

    Move __DEPTH__ setting from bh0 to here.

--- activate.sh
+++ activate.sh
@@ -1,8 +1,13 @@
+bh0firstrun && bh0export  __DEPTH__  -1
+true        && bh0export  __DEPTH__  $(( ${__DEPTH__} + 1 ))
+
 function depth {} {
     echo ${__DEPTH__}
 }
+
 function exit {
     echo "leaving depth #${__DEPTH__}"
     [[ "${__DEPTH__}" == "0" ]] || builtin exit
     echo try 'builtin exit'
 }
+
